ba13c4c459ed87cebc6a5c9920c9585cbabb0b29
zhangyuchen <823731949@qq.com>
2024-05-04 16:03:04 +0800

    init commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/entities/PayDTO.java
@@ -0,0 +1,29 @@
+package com.atguigu.cloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+/**
+ * @ClassName PayDTO
+ * @Author Rain
+ * @Date 2024/5/4 15:03
+ * @Version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PayDTO implements Serializable
+{
+    private Integer id;
+    //支付流水号
+    private String payNo;
+    //订单流水号
+    private String orderNo;
+    //用户账号ID
+    private Integer userId;
+    //交易金额
+    private BigDecimal amount;
+}
---
+++ mybatis_generator2024/README.md
@@ -0,0 +1,6 @@
+# spring-cloud-learning
+
+## docker run mysql8
+```shell
+$ docker run -itd --name mysql-test -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql:8
+```
