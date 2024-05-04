489db8b257635d27f193ed171061a6f71457f749
zhangyuchen <823731949@qq.com>
2024-05-04 16:39:12 +0800

    add swagger config

--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/controller/PayController.java
@@ -3,6 +3,8 @@ package com.atguigu.cloud.controller;
 import com.atguigu.cloud.entities.Pay;
 import com.atguigu.cloud.entities.PayDTO;
 import com.atguigu.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
@@ -16,19 +18,23 @@ import java.util.List;
  * @Version 1.0
  */
 @RestController
+@Tag(name = "支付微服务模块",description = "支付CRUD")
 public class PayController{
     @Resource PayService payService;
     @PostMapping(value = "/pay/add")
+    @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
     public String addPay(@RequestBody Pay pay){
         System.out.println(pay.toString());
         int i = payService.add(pay);
         return "成功插入记录，返回值："+i;
     }
     @DeleteMapping(value = "/pay/del/{id}")
+    @Operation(summary = "按照ID删除",description = "删除支付流水方法")
     public Integer deletePay(@PathVariable("id") Integer id) {
         return payService.delete(id);
     }
     @PutMapping(value = "/pay/update")
+    @Operation(summary = "修改",description = "修改支付流水方法")
     public String updatePay(@RequestBody PayDTO payDTO){
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
@@ -37,11 +43,13 @@ public class PayController{
         return "成功修改记录，返回值："+i;
     }
     @GetMapping(value = "/pay/get/{id}")
+    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
     public Pay getById(@PathVariable("id") Integer id){
         return payService.getById(id);
     }
 
     @GetMapping(value = "/pay/getAll")
+    @Operation(summary = "获取所有流水",description = "查询所有支付流水方法")
     public List<Pay> getAll(){
         return payService.getAll();
     }
--- cloud-provider-payment8001/src/main/java/com/atguigu/cloud/entities/Pay.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/cloud/entities/Pay.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloud.entities;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,6 +14,7 @@ import java.util.Date;
  * 表注释：支付交易表
 */
 @Table(name = "t_pay")
+@Schema(title = "支付交易表Entity")
 public class Pay {
     @Id
     @GeneratedValue(generator = "JDBC")
@@ -21,18 +24,21 @@ public class Pay {
      * 支付流水号
      */
     @Column(name = "pay_no")
+    @Schema(title = "支付流水号")
     private String payNo;
 
     /**
      * 订单流水号
      */
     @Column(name = "order_no")
+    @Schema(title = "订单流水号")
     private String orderNo;
 
     /**
      * 用户账号ID
      */
     @Column(name = "user_id")
+    @Schema(title = "用户账号ID")
     private Integer userId;
 
     /**
