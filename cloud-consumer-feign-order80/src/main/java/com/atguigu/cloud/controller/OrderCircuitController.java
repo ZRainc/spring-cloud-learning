10ddcb4cb21b3184b1908d4097320d0a9dc08d27
zhangyuchen <823731949@qq.com>
2024-05-08 20:02:05 +0800

    Add Semaphore Bulkhead

--- cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderCircuitController.java
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderCircuitController.java
@@ -1,6 +1,7 @@
 package com.atguigu.cloud.controller;
 
 import com.atguigu.cloud.apis.PayFeignApi;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,5 +28,14 @@ public class OrderCircuitController {
     public String myCircuitFallback(Integer id,Throwable t) {
         // 这里是容错处理逻辑，返回备用结果
         return "myCircuitFallback，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
+    }
+
+    @GetMapping(value = "feign/pay/bulkhead/{id}")
+    @Bulkhead(name = "cloud-payment-service", fallbackMethod = "myBulkheadFallback")
+    public String myBulkHead(@PathVariable("id") Integer id) {
+        return payFeignApi.myBulkhead(id);
+    }
+    public String myBulkheadFallback(Integer id,Throwable t) {
+        return "myBulkheadFallback，隔板超出最大数量限制，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
     }
 }
