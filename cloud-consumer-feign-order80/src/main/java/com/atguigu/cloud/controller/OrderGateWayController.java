d3f4e43e711c1732213c00edf22ffbf22424bb1a
zhangyuchen <823731949@qq.com>
2024-05-08 20:58:05 +0800

    Add Spring Cloud Gateway

---
+++ cloud-consumer-feign-order80/src/main/java/com/atguigu/cloud/controller/OrderGateWayController.java
@@ -0,0 +1,32 @@
+package com.atguigu.cloud.controller;
+
+import com.atguigu.cloud.apis.PayFeignApi;
+import com.atguigu.cloud.response.ResultData;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName OrderGateWayController
+ * @Author Rain
+ * @Date 2024/5/8 20:43
+ * @Version 1.0
+ */
+@RestController
+public class OrderGateWayController
+{
+    @Resource
+    private PayFeignApi payFeignApi;
+
+    @GetMapping(value = "/feign/pay/gateway/get/{id}")
+    public ResultData getById(@PathVariable("id") Integer id) {
+        return payFeignApi.getById(id);
+    }
+
+    @GetMapping(value = "/feign/pay/gateway/info")
+    public ResultData<String> getGatewayInfo() {
+        return payFeignApi.getGatewayInfo();
+    }
+}
+
