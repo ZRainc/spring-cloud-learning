ab41ffb275a940462f67194373ac85b92c3adcd5
zhangyuchen <823731949@qq.com>
2024-05-23 22:39:09 +0800

    add cloudalibaba consumer nacos order module

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/config/RestTemplateConfig.java
@@ -0,0 +1,23 @@
+package com.atguigu.cloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName RestTemplateConfig
+ * @Author Rain
+ * @Date 2024/5/23 22:27
+ * @Version 1.0
+ */
+@Configuration
+public class RestTemplateConfig
+{
+    @Bean
+    @LoadBalanced //赋予RestTemplate负载均衡的能力
+    public RestTemplate restTemplate()
+    {
+        return new RestTemplate();
+    }
+}
