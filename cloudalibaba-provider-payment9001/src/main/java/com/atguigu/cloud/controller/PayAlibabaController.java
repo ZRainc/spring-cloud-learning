24e6431af2a0a74edd229e7fb7ba32dad6371b87
zhangyuchen <823731949@qq.com>
2024-05-23 22:21:08 +0800

    add cloudalibaba provider payment module

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/cloud/controller/PayAlibabaController.java
@@ -0,0 +1,26 @@
+package com.atguigu.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName PayAlibabaController
+ * @Author Rain
+ * @Date 2024/5/23 22:14
+ * @Version 1.0
+ */
+
+@RestController
+public class PayAlibabaController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/pay/nacos/{id}")
+    public String getPayInfo(@PathVariable("id") Integer id)
+    {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+}
