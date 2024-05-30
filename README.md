a9518ed601d4b82ec941ee2d5371bb51a6698cf0
zhangyuchen <823731949@qq.com>
2024-05-30 21:15:14 +0800

    add cloudalibaba sentinel service module

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ docker volume create consul-data
 
 docker run -d --name=consul -p 8500:8500 -p 8600:8600/udp -v consul-data:/consul/data consul:1.15.2 agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
 ```
+
+## nacos
+```shell
+sh startup.sh -m standalone
+```
+http://localhost:8848
+
+## sentinel
+```shell
+java -jar sentinel-dashboard-1.8.6.jar
+```
+http://localhost:8080
+
+sentinel/sentinel
---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/handler/MyRequestOriginParser.java
@@ -0,0 +1,20 @@
+package com.atguigu.cloud.handler;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName MyRequestOriginParser
+ * @Author Rain
+ * @Date 2024/5/30 20:57
+ * @Version 1.0
+ */
+@Component
+public class MyRequestOriginParser implements RequestOriginParser
+{
+    @Override
+    public String parseOrigin(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameter("serverName");
+    }
+}
